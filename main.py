f677e4712fab1af5df2fb6010ba57e1de9a78693
aaRock766 <aarock766@gmail.com>
2024-06-26 14:06:26 +0800

    20240626 Office 函數

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-T='hjkf;l'
+import random
+balance = 10000
+emploee = 20
 
-for x in T:
-    print(x)
+while balance > 0:
+    for i in range(1,21):
+        print(f"第{i}員工")
+        perfor = random.randint(1,10)
+        print(f"績效:{perfor}")
+        if perfor <5:
+            print("績效不好，沒有薪水")
+            continue
+        elif balance<=0:
+            print("沒錢了")
+            break
+        else:
+            print("發放1000元")
+            balance = balance-400
+        print(f"剩餘{balance}")
