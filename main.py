e0c1fa8902b92ca889968e2d27b81eefe3add8f4
aaRock766 <aarock766@gmail.com>
2024-06-27 15:17:09 +0800

    20240627 練習函數

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-import random
-balance = 10000
-emploee = 20
+n = 200
 
-while balance > 0:
-    for i in range(1,21):
-        print(f"第{i}員工")
-        perfor = random.randint(1,10)
-        print(f"績效:{perfor}")
-        if perfor <5:
-            print("績效不好，沒有薪水")
-            continue
-        elif balance<=0:
-            print("沒錢了")
-            break
-        else:
-            print("發放1000元")
-            balance = balance-400
-        print(f"剩餘{balance}")
+def a():
+    n=50
+    print(n)
+
+def b():
+    global n
+    n=100
+    print(n)
+
+a()
+b()
+print(n)
