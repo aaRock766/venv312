013c6cd552a9222c38be5c47b60378c15a680151
rock <aarock766@gmail.com>
2024-07-18 15:04:46 +0800

    20240718 test modul

--- Test20240701.py
+++ Test20240701.py
@@ -1,30 +1,5 @@
 # -*- coding:utf-8 -*-
-stu_score_dic={
-    "王力宏":{
-        "語文":77,
-        "數學":66,
-        "英文":33
-    }, "周杰倫":{
-        "語文":88,
-        "數學":86,
-        "英文":55
-    }, "林俊傑":{
-        "語文": 99,
-        "數學": 96,
-        "英文": 66
-    }
-}
-
-print(f"可是訊息{stu_score_dic}")
-r = stu_score_dic["王力宏"]["語文"]
-print(r)
-
-stu_score_dic["吳中石"]:33
-print(stu_score_dic)
-
-stu_score_dic={
-    "王力宏":77,
-    "周杰倫":88
-    "林俊傑":12}
-stu_score_dic.clear()
-print(stu_score_dic)
+from time import sleep
+print("su")
+time.sleep(5)
+print("aa")
