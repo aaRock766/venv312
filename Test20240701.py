1fe0648d69df2d2f57ba84bd2a10d9c29634c0eb
rock <aarock766@gmail.com>
2024-08-12 16:01:16 +0800

    20240812 Office

--- Test20240701.py
+++ Test20240701.py
@@ -1,5 +1,15 @@
 # -*- coding:utf-8 -*-
-from time import sleep
-print("su")
-time.sleep(5)
-print("aa")
+import pyecharts
+from pyecharts.charts import Line
+from pyecharts.options import TitleOpts
+line=Line()
+line.add_xaxis(["中國","美國","法國"])
+line.add_yaxis("GDP",[30,10,20])
+
+
+line.set_global_opts(
+    title_opts=TitleOpts(title="GDP展示", pos_left="center", pos_bottom="2%")
+)
+
+
+line.render()
