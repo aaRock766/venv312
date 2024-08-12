21b3be2b5de5cad5468befb1071479909547ce81
aarock766 <aarock766@gmail.com>
2024-08-12 22:03:05 +0800

    20240812 home night

--- file_define.py
+++ file_define.py
@@ -1,6 +1,8 @@
 """
 定義文件相關的類
 """
+import json
+
 from data_define import Record
 
 
@@ -14,13 +16,42 @@ class TextFileReader(FileReader):
         self.path = path
 
     def read_data(self) -> list[Record]:
-        f = open(self.path, "r", encoding="UTF-8")
+        f = open(self.path, "r", encoding="UTF-8-sig")
+        record_list = []
         for line in f.readlines():
             line = line.strip("\n")
+            # print(line)
             data_list = line.split(",")
-            print(data_list)
+            # print(data_list)
+            record = Record(data_list[0], data_list[1], int(data_list[2]), data_list[3])
+            record_list.append(record)
+        f.close()
+        return record_list
+
+
+class JsonFileReader(FileReader):
+
+    def __init__(self, path):
+        self.path = path
+
+    def read_data(self) ->list[Record]:
+        f = open(self.path, "r", encoding="UTF-8-sig")
+        record_list = []
+
+        for line in f.readlines():
+            data_dict = json.loads(line)
+            record = Record(data_dict["date"], data_dict["order_id"], int(data_dict["money"]), data_dict["province"])
+            record_list.append(record)
+
+
+        f.close()
+        return record_list
 
 
 if __name__ == '__main__':
-    text_file_reader = TextFileReader("Image_006.txt")
-    text_file_reader.read_data()
+    text_file_reader = TextFileReader("image_006.txt")
+    json_file_reader = JsonFileReader("image_007.txt")
+    list1 = text_file_reader.read_data()
+    list2 = json_file_reader.read_data()
+    for l in list1:
+        print(l)
